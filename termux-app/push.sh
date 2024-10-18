cca1699f0e817427979c82b3ab30c90a9c36558b
Hayin FX <hayinfx@yahoo.com>
2024-10-18 07:20:47 +0000

    -

--- termux-app/push.sh
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "TerminClient v0.0.1 Beta rev1 Commit Update rev 1"
-git push -u origin main
--- termux-app/upTempApk.sh
+++
@@ -1,1 +0,0 @@
-curl -F "file=@app/build/outputs/apk/debug/termux-app_apt-android-7-debug_arm64-v8a.apk" https://temp.sh/upload
